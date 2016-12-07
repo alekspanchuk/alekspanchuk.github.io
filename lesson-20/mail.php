577bc0f7d4d7c18fea3eeef63cae993ebff4c04f
Aleksandr Panchuk <alekspanchuk@yandex.ru>
2016-12-07 19:01:57 +0500

    lesson-20 complete

--- lesson-20/mail.php
+++ lesson-20/mail.php
@@ -1,42 +1,21 @@
-<?php 
-$from = $_POST['from'];
-$name = $_POST['name'];
-$email = $_POST['email'];
-$text = $_POST['text'];
-
-require_once('phpmailer/PHPMailerAutoload.php');
-$mail = new PHPMailer;
-$mail->CharSet = 'utf-8';
-
-//$mail->SMTPDebug = 3;                               // Enable verbose debug output
-
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'alex88temp@mail.ru';                 // SMTP username
-$mail->Password = 'Qawsedrftg';                           // SMTP password
-$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 465;                                    // TCP port to connect to
-
-$mail->setFrom('mail@homework.sl', 'User');
-$mail->addAddress('alekspanchuk@yandex.ru', 'Alex Panchuk');     // Add a recipient
-//$mail->addAddress('ellen@example.com');               // Name is optional
-//$mail->addReplyTo('info@example.com', 'Information');
-//$mail->addCC('cc@example.com');
-//$mail->addBCC('bcc@example.com');
-//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-$mail->isHTML(true);                                  // Set email format to HTML
-
-$mail->Subject = 'Это тема сообщения';
-$mail->Body    = 'Это тело сообщения дальше жирное <b>блаблабла</b>';
-$mail->AltBody = 'Это сообщение в формате plain text';
-
-if(!$mail->send()) {
-    echo 'Message could not be sent.';
-    echo 'Mailer Error: ' . $mail->ErrorInfo;
-} else {
-    echo 'Message has been sent';
-}
-
+<?php
+	//Принимаем постовые данные
+	$name=$_POST['name'];
+	$phone=$_POST['phone'];
+	//Тут указываем на какой ящик посылать письмо
+	$to = "79991570874@yandex.ru";
+	//Далее идет тема и само сообщение
+	// Тема письма
+	$subject = "Заявка с сайта";
+	// Сообщение письма
+	$message = "
+	Имя пользователя: ".htmlspecialchars($name)."<br />
+	Телефон: <a href='tel:$phone'>".htmlspecialchars($phone)."</a>";
+	$headers = "From: stastroi.ru <mail@stastroi.ru>\r\nContent-type: text/html; charset=UTF-8 \r\n";
+	// Отправляем письмо при помощи функции mail();
+	mail ($to, $subject, $message, $headers);
+	// Перенаправляем человека на страницу благодарности и завершаем скрипт
+	// header('Location: index.html');
+	echo('Thanks');
+	exit();
 ?>
