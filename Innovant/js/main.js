6049e84f39d9a1960a008686a015f8c651354a5d
Aleksandr Panchuk <alekspanchuk@yandex.ru>
2017-02-01 23:50:32 +0500

    make menu workd and about button provide to portfolio

--- Innovant/js/main.js
+++ Innovant/js/main.js
@@ -1,9 +1,46 @@
-$(document).ready(function(){
-// ABOUT HEIGHT DEFINE
-  console.log('Hello world');
-  
-  
+$().ready(function(){
 
+// MENU
+  var menu = $('.hamburger__menu_list');
+  // show
+    $('.header__hamburger').on('click', function(event) {
+      menu.show();
+    });
+  // hide on click out of menu
+    $(document).mouseup(function (e) {
+      if (menu.has(e.target).length === 0){
+          menu.hide();
+      }
+    });
+  // Smooth transition
+    // for top menu
+    $(".hamburger__menu_item").on("click","a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+        menu.hide();
+        //забираем идентификатор блока с атрибута href
+        var id  = $(this).attr('href'),
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+        //анимируем переход на расстояние - top за 700 мс
+        $('body,html').animate({scrollTop: top}, 700);
+    });
+    // for hamburger menu
+    $(".header__menu_item").on("click","a", function (event) {
+        event.preventDefault();
+        menu.hide();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 700);
+    });
+// ABOUT
+    $(".content__button").on("click", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 700);
+    });
+// 
 // FEEDBACK SLIDERS
   $('.fback__slider').slick({
   	infinite: true,
@@ -52,5 +89,7 @@ $(document).ready(function(){
     ]
 
   });
+// WOW
+  new WOW().init();
 
 });
